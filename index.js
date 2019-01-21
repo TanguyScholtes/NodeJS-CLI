077f935091a0f497973bff881e24838ff378a0e7
TanguyScholtes <soultaker720@hotmail.com>
2019-01-21 14:47:10 +0100

    added figlet with Bloody font

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 #!/usr/bin/env node
-//console.log( "Hello, Node.JS!" ); // display in bash with "node ." or "npm run emailChecker" as specified in package.json
 
+// get in from bash with "node ." or "npm run emailChecker" as specified in package.json
 const emailValidator = require( 'email-validator' );
 const axios = require( 'axios' );
 const chalk = require( 'chalk' );
+const figlet = require( 'figlet' );
+
 let userEntry = process.argv[ 2 ];
+
+/*
+// display figlet's fonts list
+figlet.fonts(function(err, fonts) {
+    if (err) {
+        console.log('something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.dir(fonts);
+});
+return;
+*/
+
+figlet( "BeCode\nEmail Checker",
+    {
+        kerning: 'fitted',
+        font: 'Doom'
+    },
+    function( error, string ) {
+        if ( error ) {
+            console.log( 'Whoops!' );
+            console.dir( error );
+            return;
+        }
+        console.log( string );
+    }
+);
 
 if ( emailValidator.validate( userEntry ) ) {
     // contact pwned with verified email address
@@ -18,16 +48,16 @@ if ( emailValidator.validate( userEntry ) ) {
         responseType: 'json'
     } )
         .then( function ( response ) {
-            console.log( chalk.white( chalk.bold.blue( userEntry ) + ' has ' + chalk.bold.red( response.data.length + ' breaches' ) + ' (out of 340) on the following domains :\n' ) );
+            console.log( chalk.white( chalk.bold.cyan( userEntry ) + ' has ' + chalk.bold.red( response.data.length + ' breaches' ) + ' (out of 340) on the following domains :\n' ) );
             response.data.forEach( element => console.log( chalk.red( element.Name ) ) );
         } )
         .catch( function ( error ) {
             if ( error.response.status === 404 ) {
-                console.log( chalk.green( userEntry + ' doesn\'t have any breach. Congratulations !' ) );
+                console.log( chalk.green( chalk.bold.cyan( userEntry ) + ' doesn\'t have any breach. Congratulations !' ) );
             } else {
-                console.log( chalk.red( 'Breaches check on ' + userEntry + ' failed with error code ' + error.response.status ) );
+                console.log( chalk.red( 'Breaches check on ' + chalk.bold.cyan( userEntry ) + ' failed with error code ' + error.response.status ) );
             }
         } );
 } else {
-    console.log( chalk.red( userEntry + " is not valid. Please retry with a valid email adress." ) );
+    console.log( chalk.red( chalk.bold.cyan( userEntry ) + " is not valid. Please retry with a valid email adress." ) );
 }
