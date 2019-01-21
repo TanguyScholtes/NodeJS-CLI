21a94da9e09984bd92cafe18be72528291235b9a
TanguyScholtes <soultaker720@hotmail.com>
2019-01-21 15:12:53 +0100

    added ora spinner

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const emailValidator = require( 'email-validator' );
 const axios = require( 'axios' );
 const chalk = require( 'chalk' );
 const figlet = require( 'figlet' );
+const ora = require( 'ora' );
 
 let userEntry = process.argv[ 2 ];
 
@@ -25,15 +26,18 @@ figlet( "BeCode\nEmail Checker",
 
 function validateEmail ( email ) {
     if ( emailValidator.validate( email ) ) {
-        // contact pwned with verified email address
-        // display result
+        // Email is valid
         checkBreaches( email );
     } else {
+        // Email is not valid
         console.log( chalk.red( chalk.bold.cyan( email ) + " is not valid. Please retry with a valid email adress." ) );
     }
 }
 
 function checkBreaches( email ) {
+    // Start spinner while requesting API
+    const spinner = ora( 'Checking for breaches...\n' ).start();
+
     axios( {
         method: 'get',
         baseURL: 'https://haveibeenpwned.com/api/v2/breachedaccount/' + email + '?truncateResponse=true',
@@ -43,16 +47,25 @@ function checkBreaches( email ) {
         responseType: 'json'
     } )
         .then( function ( response ) {
+            // Stop spinner
+            spinner.succeed( 'Checked !\n' );
+            spinner.stop();
+
+            // Display results
             console.log( chalk.white( chalk.bold.cyan( email ) + ' has ' + chalk.bold.red( response.data.length + ' breaches' ) + ' (out of 340) on the following domains :\n' ) );
             response.data.forEach( element => console.log( chalk.red( element.Name ) ) );
         } )
         .catch( function ( error ) {
+            // API returns 404 error when no breach is found
             if ( error.response.status === 404 ) {
+                // If error is 404
                 console.log( chalk.green( chalk.bold.cyan( email ) + ' doesn\'t have any breach. Congratulations !' ) );
             } else {
+                // If error is something else than 404
                 console.log( chalk.red( 'Breaches check on ' + chalk.bold.cyan( email ) + ' failed with error code ' + error.response.status ) );
             }
         } );
 }
 
+// Start email validation
 validateEmail( userEntry );
